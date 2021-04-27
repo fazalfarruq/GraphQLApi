a8edc6f22287560cf26cdec7814cd34992cb8498
Fazal Farruq <FazalF@mmfoodmarket.com>
2021-04-27 18:05:07 -0400

    removed strong types from schema

--- GraphQLApi/Schema/ProductSchema.cs
+++ GraphQLApi/Schema/ProductSchema.cs
@@ -4,15 +4,16 @@ using System.Linq;
 using System.Threading.Tasks;
 using GraphQLApi.Mutation;
 using GraphQLApi.Query;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace GraphQLApi.Schema
 {
     public class ProductSchema : GraphQL.Types.Schema
     {
-        public ProductSchema(ProductQuery productQuery, ProductMutation productMutation)
+        public ProductSchema(IServiceProvider provider)
         {
-            Query = productQuery;
-            Mutation = productMutation;
+            Query = provider.GetRequiredService<ProductQuery>();
+            Mutation = provider.GetRequiredService<ProductMutation>();
         }
     }
 }
