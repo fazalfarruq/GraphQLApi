836bcfd3c79833507c193a4ba83645161e66d767
Fazal Farruq <FazalF@mmfoodmarket.com>
2021-04-27 11:49:35 -0400

    added ef and updated product service

---
+++ GraphQLApi/Data/GraphQlDbContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using GraphQLApi.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace GraphQLApi.Data
+{
+    public class GraphQlDbContext : DbContext
+    {
+        public GraphQlDbContext(DbContextOptions<GraphQlDbContext> options) : base(options)
+        {
+            
+        }
+
+        public DbSet<Product> Products { get; set; }
+    }
+}
--- GraphQLApi/Services/ProductService.cs
+++ GraphQLApi/Services/ProductService.cs
@@ -1,4 +1,7 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using GraphQLApi.Data;
 using GraphQLApi.Interfaces;
 using GraphQLApi.Models;
 
@@ -6,40 +9,44 @@ namespace GraphQLApi.Services
 {
     public class ProductService : IProductService
     {
-        private static List<Product> products = new List<Product>
-        {
-            new Product(){Id = 1,Name = "Coffee",Price= 10},
-            new Product(){Id = 2,Name = "Tea",Price= 15},
+        private readonly GraphQlDbContext _dbContext;
 
-        };
+        public ProductService(GraphQlDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public Product GetProductById(int id)
+        {
+            return _dbContext.Products.Find(id);
+        }
+
         public List<Product> GetAllProducts()
         {
-            return products;
+            return _dbContext.Products.ToList();
         }
 
         public Product AddProduct(Product product)
         {
-            products.Add(product);
+            _dbContext.Products.Add(product);
+            _dbContext.SaveChanges();
             return product;
         }
 
         public Product UpdateProduct(int id, Product product)
         {
-            var tempProduct = GetProductById(id);
-            var index = products.FindIndex(x => x.Id.Equals(tempProduct.Id));
-            products[index] = product;
+            var productObj = GetProductById(id);
+            productObj.Name = product.Name;
+            productObj.Price = product.Price;
+            _dbContext.SaveChanges();
             return product;
         }
 
         public void DeleteProduct(int id)
         {
             var productToRemove = GetProductById(id);
-            products.Remove(productToRemove);
-        }
-
-        public Product GetProductById(int id)
-        {
-            return products.Find(x => x.Id == id);
+            _dbContext.Products.Remove(productToRemove);
+            _dbContext.SaveChanges();
         }
     }
 }
